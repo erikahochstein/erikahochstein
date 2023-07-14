ce478eb16f54af9337b73322ea26abd5cae8c026
ehoch <ehoch@umich.edu>
2023-07-14 11:17:36 -0400

    navigation styling

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function revealBreak() {
+    var reveals = document.querySelector(".reveal-break");
+    // Determine position of element from top of viewport
+    var viewportHeight = window.innerHeight;
+    var distTop = reveals.getBoundingClientRect().top;
+    console.log(distTop);
+
+    // Reveal when distance from top of viewport is 150
+    var becomeVisible = 150;
+    // if (distTop < (viewportHeight - becomeVisible))
+}
+
+window.addEventListener("scroll", revealBreak);
+
+// Navigation Toggle
+jQuery(document).ready(function() {
+    jQuery('.toggle-nav').click(function(e) {
+        jQuery(this).toggleClass('active');
+        jQuery('.menu-list').toggleClass('active');
+
+        e.preventDefault();
+    });
+});
